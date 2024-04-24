a96634f9b396c558d7c59d6f5f051174f616e55e
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2024-04-24 18:31:19 -0500

    parameterize and consume the custom Hook useFetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,7 +1,17 @@
 import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
-  useFetch();
+  const { data, hasError, isLoading } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon/4"
+  );
 
-  return <div>MultipleCustomHooks</div>;
+  return (
+    <>
+      <h1>Informacion del Pokemon</h1>
+      <hr />
+      {isLoading && <p>Cargando...</p>}
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <h2>{data?.name}</h2>
+    </>
+  );
 };
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,53 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = () => {
+export const useFetch = (url) => {
   const [state, setState] = useState({
     data: null,
     isLoading: true,
     hasError: false,
-    errorMessage: null,
+    error: null,
   });
 
   useEffect(() => {
     getFetch();
-  }, []);
+  }, [url]);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  };
 
   const getFetch = async () => {
-    const resp = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    setLoadingState();
+    const resp = await fetch(url);
+
+    // sleep
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    if (!resp.ok) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: resp.status,
+          message: resp.statusText,
+        },
+      });
+      return;
+    }
+
     const data = await resp.json();
-    console.log("🚀 ~ getFetch ~ data:", data);
+    setState({
+      data: data,
+      isLoading: false,
+      hasError: false,
+      error: null,
+    });
   };
 
   return {
