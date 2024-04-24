93b9d2237199dffae05cb84818d0cd71175902ed
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2024-04-24 17:28:24 -0500

    useEffect - precautions

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -2,10 +2,15 @@ import { useEffect } from "react";
 
 export const Message = () => {
   useEffect(() => {
-    console.log("Message Mounted");
+    const onMouseMove = ({ x, y }) => {
+      const coords = { x, y };
+      console.log("🚀 ~ mouseMove ~ coords:", coords);
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
 
     return () => {
-      console.log("Message unMounted");
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
