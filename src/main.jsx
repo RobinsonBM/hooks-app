55aa7c5ff8f6d7c723d09f666bce65f2e250fef7
Robinson Betancur Marin <robetancur.mv@gmail.com>
2024-05-27 22:29:38 -0500

    useMemo Hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import './index.css';
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -8,11 +8,11 @@ import "./index.css";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Memorize } from "./06-memos/Memorize";
+import { MemoHook } from './06-memos/MemoHook';
 
-import { Memorize } from "./06-memos/Memorize";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <Memorize />
+  <MemoHook />,
   // </React.StrictMode>
 );
