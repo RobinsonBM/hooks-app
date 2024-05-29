e419be88ce331b093278252be8b3855a3e978620
Robinson Betancur Marin <robetancur.mv@gmail.com>
2024-05-28 21:06:09 -0500

    memorize task

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,11 @@ import './index.css';
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
-import {
-  // Memorize,
-  // MemoHook,
-  CallbackHook,
-} from './06-memos';
+// import { Memorize, MemoHook, CallbackHook } from './06-memos';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <CallbackHook />,
+  <Padre />,
   // </React.StrictMode>
 );
