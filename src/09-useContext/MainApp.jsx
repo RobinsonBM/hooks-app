1efbe172cdc675c63f2ef00fb4ea32f0d3068e4f
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2024-06-06 16:11:53 -0500

    Link Router

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,19 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { HomePage, AboutPage, LoginPage } from './components';
+import { HomePage, AboutPage, LoginPage, Navbar } from './components';
 
 export const MainApp = () => {
   return (
     <>
       <h1>Main App</h1>
+      <Navbar />
       <hr />
 
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='about' element={<AboutPage />} />
-        <Route path='login' element={<LoginPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/login' element={<LoginPage />} />
 
         {/* <Route path='/*' element={<LoginPage />} /> */}
-        <Route path='*' element={<Navigate to='about' />} />
+        <Route path='*' element={<Navigate to='/about' />} />
       </Routes>
     </>
   );
